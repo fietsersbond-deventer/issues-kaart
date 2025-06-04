e56bc96e4a8158daf8e1685a4ce0176a969203c0
Arjen Haayman <arjen.haayman@gmail.com>
2025-06-04 18:27:17 +0200

    morgan geeft foutmelding in cloudflare

--- server/middleware/log.ts
+++
@@ -1,6 +0,0 @@
-import morgan from "morgan";
-
-export default defineEventHandler(async (event) => {
-  const logger = morgan("common");
-  logger(event.node.req, event.node.res, function () {});
-});
