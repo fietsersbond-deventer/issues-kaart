3135ff4408c4c2bbbdb41825e2d5b3f097280773
Arjen Haayman <arjen.haayman@gmail.com>
2025-06-08 11:12:41 +0200

    voorkom dat 'new' issue wordt gebruikt voor bbox

--- app/utils/getIssuesBbox.ts
+++ app/utils/getIssuesBbox.ts
@@ -9,14 +9,15 @@ function transformBboxToOpenLayers(bbox: BBox): BBox {
   // Convert to OpenLayers coordinates
   const min = fromLonLat([minX, minY]);
   const max = fromLonLat([maxX, maxY]);
-  return [min[0]!, min[1], max[0], max[1]];
+  return [min[0]!, min[1]!, max[0]!, max[1]!];
 }
 
 export function getIssuesBbox(issues: Issue[]): BBox | undefined {
-  if (issues.length === 0) return undefined;
+  const existingIssies = issues.filter((issue) => issue.id);
+  if (existingIssies.length === 0) return undefined;
 
   // Create a feature collection from the geometries
-  const features = issues.map((issue) => ({
+  const features = existingIssies.map((issue) => ({
     type: "Feature" as const,
     properties: {},
     geometry: issue.geometry,
