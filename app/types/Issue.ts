2d496b3ec0bf61ab1eaaf6d7c1354fb05cfecd6d
Arjen Haayman <arjen.haayman@gmail.com>
2025-06-10 20:05:28 +0200

    geometry is verplicht in EditForm

--- app/types/Issue.ts
+++ app/types/Issue.ts
@@ -1,7 +1,9 @@
 import type { LineString, Point, Polygon } from "geojson";
 
-export type Issue = {
-  id?: number;
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type ExistingIssue = {
+  id: number;
   title: string;
   description: string;
   legend_id: number | null;
@@ -10,3 +12,18 @@ export type Issue = {
   geometry: Point | Polygon | LineString;
   created_at?: Date;
 };
+
+export type NewIssue = Optional<
+  Omit<ExistingIssue, "id" | "created_at">,
+  "geometry"
+>;
+
+export type Issue = ExistingIssue | NewIssue;
+
+export function isExistingIssue(issue: Issue): issue is ExistingIssue {
+  return "id" in issue;
+}
+
+export function isNewIssue(issue: Issue): issue is NewIssue {
+  return !("id" in issue);
+}
