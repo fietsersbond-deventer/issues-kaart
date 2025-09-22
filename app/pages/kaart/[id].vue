02c53e406080a93c90aa23d43bbf318055580c98
Arjen Haayman <arjen.haayman@gmail.com>
2025-09-22 19:30:03 +0200

    verwijder debug-log

--- app/pages/kaart/[id].vue
+++ app/pages/kaart/[id].vue
@@ -2,7 +2,6 @@
   <div class="wrapper">
     <v-toolbar v-if="status === 'authenticated'">
       <Toolbar>
-        locked: {{ editingUsers }}
         <v-btn
           v-if="status === 'authenticated'"
           :icon="!isEditing ? 'mdi-pencil' : 'mdi-pencil-remove'"
@@ -69,7 +68,6 @@ const { status } = useAuth();
 const showEditDialog = ref(false);
 const { isEditing, setEditing, toggleEditing } = useIsEditing();
 const { issue } = storeToRefs(useSelectedIssue());
-const { editingUsers } = storeToRefs(useIssueLocks());
 
 // Set the page title dynamically based on the issue
 useHead(() => ({
