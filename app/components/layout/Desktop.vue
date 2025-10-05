8b158a026206429e0ef07c36884245892d72bed6
Arjen Haayman <arjen.haayman@gmail.com>
2025-10-05 14:35:21 +0200

    gescheiden opmaak mobiel/desktop

---
+++ app/components/layout/Desktop.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout class="desktop-layout">
+    <v-main class="map-main" :style="{ width: 'calc(100% - 600px)' }">
+      <Map class="fill-height" />
+    </v-main>
+
+    <v-navigation-drawer
+      location="right"
+      width="600"
+      app
+      disable-route-watcher
+      persistent
+    >
+      <div class="navigation-content">
+        <div class="main-content">
+          <NuxtPage />
+        </div>
+      </div>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
+
+<style scoped>
+.desktop-layout {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.map-main {
+  transition: width 0.3s ease;
+}
+</style>
