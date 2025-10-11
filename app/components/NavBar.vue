45c07911847c1d016853252a72fa68eaa73bbf72
Arjen Haayman <arjen.haayman@gmail.com>
2025-10-11 11:46:20 +0200

    Gele kleur verplaatst van H1 naar navbar. Andere knoppen als ingelogd

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar>
-    <NuxtLink to="/" class="d-flex align-center">
+  <v-app-bar class="navbar">
+    <NuxtLink to="/" class="d-flex align-center logo">
       <img
         src="/fietsersbond-logo.webp"
         alt="Fietsersbond"
@@ -10,18 +10,6 @@
       <div class="header-link ml-3">Fietsersbond Deventer onderwerpen</div>
     </NuxtLink>
     <v-spacer />
-    <v-btn
-      v-tooltip:top="'Deventer Fietsersbond'"
-      href="https://deventer.fietsersbond.nl/"
-      variant="text"
-      icon="mdi-home"
-    />
-    <v-btn
-      v-tooltip:top="'Contact'"
-      href="https://deventer.fietsersbond.nl/contact/contact-met-de-afdeling/"
-      variant="text"
-      icon="mdi-email"
-    />
     <template v-if="status === 'authenticated'">
       <v-btn
         v-tooltip:top="'Nieuw onderwerp toevoegen'"
@@ -42,13 +30,26 @@
         @click="handleLogout"
       />
     </template>
-    <v-btn
-      v-else
-      v-tooltip:top="'Inloggen beheer'"
-      to="/login"
-      variant="text"
-      icon="mdi-login"
-    />
+    <template v-else>
+      <v-btn
+        v-tooltip:top="'Deventer Fietsersbond'"
+        href="https://deventer.fietsersbond.nl/"
+        variant="text"
+        icon="mdi-home"
+      />
+      <v-btn
+        v-tooltip:top="'Contact'"
+        href="https://deventer.fietsersbond.nl/contact/contact-met-de-afdeling/"
+        variant="text"
+        icon="mdi-email"
+      />
+      <v-btn
+        v-tooltip:top="'Inloggen beheer'"
+        to="/login"
+        variant="text"
+        icon="mdi-login"
+      />
+    </template>
   </v-app-bar>
 </template>
 
@@ -68,5 +69,16 @@ async function handleLogout() {
   font-weight: 700;
   display: inline-block;
   color: black;
+}
+
+.logo {
+  background-color: white;
+  height: 100%;
+  padding-right: 1em;
+  max-width: 50%;
+}
+
+.navbar {
+  background-color: rgb(255, 213, 3) !important;
 }
 </style>
