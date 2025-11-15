56bbe5582fa3b83db3afee50a5a433c029d7536e
Arjen Haayman <arjen.haayman@gmail.com>
2025-11-15 14:13:06 +0100

    revert deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,6 +31,6 @@ rsync -avz server/database/ "$DEPLOYMENT_TARGET/server/database/"
 # ssh "${DEPLOYMENT_TARGET%%:*}" "cd ${DEPLOYMENT_TARGET##*:} && node --import tsx/esm server/database/runMigrations.ts"
 
 echo "Restarting PM2 on remote..."
-ssh "${DEPLOYMENT_TARGET%%:*}" "cd ${DEPLOYMENT_TARGET##*:} && source .env && pm2 restart $PM2_APP_NAME"
+ssh "${DEPLOYMENT_TARGET%%:*}" "/home/fietsersbond/.nvm/versions/node/v22.20.0/bin/pm2 restart fietsersbond"
 
 echo "Deployment and restart complete."
