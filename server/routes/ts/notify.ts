c16df015a834e6b0c06c5267821840edd4e2dcb8
Arjen Haayman <arjen.haayman@gmail.com>
2026-08-28 20:45:37 +0200

    linting

--- server/routes/ts/notify.ts
+++ server/routes/ts/notify.ts
@@ -7,7 +7,7 @@ import { getEmitter } from "~~/server/utils/getEmitter";
 // Helper function to create type-safe WebSocket messages
 function createNotifyMessage<T extends keyof WebSocketEvents>(
   type: T,
-  payload: WebSocketEvents[T]
+  payload: WebSocketEvents[T],
 ): string {
   return JSON.stringify({ type, payload });
 }
@@ -19,34 +19,48 @@ const connectedPeers = new Set<WebSocketPeer>();
 // This prevents N² message broadcasts with N connected peers
 const emitter = getEmitter();
 
-emitter.on("issue:created", (issue: Issue & { createdBy: string; createdByUserId: number }) => {
-  console.log(
-    "[ws/notify] Broadcasting issue:created event for issue ID:",
-    issue.id
-  );
-  const message = createNotifyMessage("issue-created", issue);
-  connectedPeers.forEach((peer) => peer.send(message));
-});
+emitter.on(
+  "issue:created",
+  (issue: Issue & { createdBy: string; createdByUserId: number }) => {
+    console.log(
+      "[ws/notify] Broadcasting issue:created event for issue ID:",
+      issue.id,
+    );
+    const message = createNotifyMessage("issue-created", issue);
+    connectedPeers.forEach((peer) => peer.send(message));
+  },
+);
 
-emitter.on("issue:modified", (issue: Issue & { modifiedBy: string; modifiedByUserId: number }) => {
-  console.log(
-    "[ws/notify] Broadcasting issue:modified event for issue ID:",
-    issue.id
-  );
-  const message = createNotifyMessage("issue-modified", issue);
-  connectedPeers.forEach((peer) => peer.send(message));
-});
+emitter.on(
+  "issue:modified",
+  (issue: Issue & { modifiedBy: string; modifiedByUserId: number }) => {
+    console.log(
+      "[ws/notify] Broadcasting issue:modified event for issue ID:",
+      issue.id,
+    );
+    const message = createNotifyMessage("issue-modified", issue);
+    connectedPeers.forEach((peer) => peer.send(message));
+  },
+);
 
-emitter.on("issue:deleted", (data: { id: number; title: string; deletedBy: string; deletedByUserId: number }) => {
-  console.log(
-    "[ws/notify] Broadcasting issue:deleted event for issue ID:",
-    data.id,
-    "deleted by:",
-    data.deletedBy
-  );
-  const message = createNotifyMessage("issue-deleted", data);
-  connectedPeers.forEach((peer) => peer.send(message));
-});
+emitter.on(
+  "issue:deleted",
+  (data: {
+    id: number;
+    title: string;
+    deletedBy: string;
+    deletedByUserId: number;
+  }) => {
+    console.log(
+      "[ws/notify] Broadcasting issue:deleted event for issue ID:",
+      data.id,
+      "deleted by:",
+      data.deletedBy,
+    );
+    const message = createNotifyMessage("issue-deleted", data);
+    connectedPeers.forEach((peer) => peer.send(message));
+  },
+);
 
 export default defineWebSocketHandler({
   open(peer: WebSocketPeer) {
